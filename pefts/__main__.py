421994cba90b669ec5e477d825d26868092c6e62
rizerphe <44440399+rizerphe@users.noreply.github.com>
2024-04-21 09:31:54 +0300

    Add huggingface dataset loading

--- pefts/__main__.py
+++ pefts/__main__.py
@@ -1,12 +1,22 @@
+from datasets import load_dataset
 import torch
 
-from pefts.dataset import RecursiveDirectoryListerDataset
+from pefts.dataset import HFDatasetIterator
 from pefts.layers.lora import LoRAPeft
 from pefts.model import Transformer
 from pefts.trainer import Trainer
 
 if __name__ == "__main__":
-    dataset = RecursiveDirectoryListerDataset("data/prompters", "gpt2")
+    from_map = {"human": "user", "gpt": "assistant"}
+    format_message = (
+        lambda x: f"{from_map.get(x['from'], x['from'])}\n{x['value']}<|endoftext|>"
+    )
+    dataset = HFDatasetIterator(
+        load_dataset("totally-not-an-llm/sharegpt-hyperfiltered-3k", "train").map(
+            lambda x: {"text": "".join(format_message(y) for y in x["conversations"])}
+        ),
+        "gpt2",
+    )
     peft = LoRAPeft(lora_rank=4)
     model = Transformer.from_pretrained("gpt2", peft=peft)
 
--- pefts/dataset.py
+++ pefts/dataset.py
@@ -1,6 +1,7 @@
 import os
 from typing import Iterator
 
+from datasets import Dataset, load_dataset
 import torch
 from transformers import AutoTokenizer
 
@@ -39,3 +40,33 @@ class RecursiveDirectoryListerDataset:
                     # from the middle of a file as well
                     for i in range(0, len(example), 512):
                         yield example[i : i + 1024].unsqueeze(0)
+
+
+class HFDatasetIterator:
+    """An iterator that iterates over a Hugging Face dataset."""
+
+    def __init__(self, dataset: str | Dataset, tokenizer_name: str) -> None:
+        """Initialize the iterator.
+
+        Args:
+            dataset: The name of the dataset to use, or the dataset itself.
+            tokenizer_name: The name of the tokenizer to use.
+        """
+        self.dataset = (
+            load_dataset(dataset, "train") if isinstance(dataset, str) else dataset
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
+    def __iter__(self) -> Iterator[torch.Tensor]:
+        """Iterate over the dataset.
+
+        Yields:
+            The tokenized contents of each example.
+        """
+        for example in self.dataset["train"]["text"]:
+            if len(example) == 0:
+                continue
+            example = torch.tensor(self.tokenizer.encode(example), dtype=torch.long)
+            if example.size(0) > 1024:
+                continue
+            yield example.unsqueeze(0)
